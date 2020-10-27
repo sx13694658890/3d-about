390923fff8d62536d975d928c7c9f832d50cb740
sxl <sxl13694658890@163.com>
2020-10-27 22:07:31 +0800

    邮箱验证

---
+++ node/emailer.js
@@ -0,0 +1,22 @@
+const nodemailer=require('nodemailer')   
+    // 创建发送对象
+    let transporter=nodemailer.createTransport({
+         host:"smtp.163.com",
+         port:465,
+         secure:true,
+         auth:{
+             user:"sxl13694658890@163.com",   
+             pass:"DTTAIPHNASFJFVZN" 
+         }
+    })
+
+   transporter.sendMail({
+         from:'sxl13694658890@163.com',
+         to:"1632241215@qq.com",
+         subject:"163",
+         text:"您的验证码是:999999,五分钟内有效",
+    },(err,data)=>{
+        console.log(err)
+        console.log(data)
+    })
+   
